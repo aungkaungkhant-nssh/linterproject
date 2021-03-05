75988a3570326f1b37437ed3a06761fb5564789b
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-03-05 20:31:25 +0630

    fetch project and  show in our home view of the single component

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="project">
+      <h3 @click="showDetail">{{project.title}}</h3>
+      <p v-if="hideDetail">{{project.detail}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['project'],
+    data(){
+        return{
+            hideDetail:false
+        }
+    },
+    methods:{
+        showDetail(){
+            this.hideDetail=!this.hideDetail;
+        }
+    }
+}
+</script>
+
+<style>
+    .project{
+        background-color: #ccc;
+        width:400px;
+        height: 70px;
+        margin:10px;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    h3{
+        color:indigo;
+        width:120px;
+        cursor: pointer;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project"></SingleProject>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject'
 // @ is an alias to /src
 
 
 export default {
   name: 'Home',
   components: {
+    SingleProject,
    
+  },
+  data(){
+    return{
+      projects:[],
+    }
+  },
+  mounted(){
+    fetch("http://localhost:3000/projects")
+    .then((response)=>{
+      return response.json();
+    })
+    .then((datas)=>{
+        this.projects=datas;
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
   }
 }
 </script>
