ed62b79bca43a1d648ec9bf44d4a43249d540eb8
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-03-06 19:37:38 +0630

    update material icon

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="project">
-      <h3 @click="showDetail">{{project.title}}</h3>
-      <p v-if="hideDetail">{{project.detail}}</p>
+      <div class="flexing">
+          <div>
+            <h3 @click="showDetail">{{project.title}}</h3>
+             <p v-if="hideDetail">{{project.detail}}</p>
+          </div>
+      
+        <div>
+          <span class="material-icons">
+            delete
+             </span>
+         <span class="material-icons">
+            edit
+            </span>
+            <span class="material-icons">
+                done
+            </span>
+         </div>
+     </div>
+  
   </div>
 </template>
 
@@ -23,16 +40,28 @@ export default {
 
 <style>
     .project{
-        background-color: #ccc;
-        width:400px;
-        height: 70px;
-        margin:10px;
-        padding: 20px;
-        border-radius: 10px;
+        padding:20px;
+        background-color: #f2f2f2;
+        cursor: pointer;
+        margin: 10px;
+        border-left: 6px solid crimson;
+        border-radius: 8px;
+       
     }
     h3{
         color:indigo;
         width:120px;
         cursor: pointer;
+    }
+    .flexing{
+        display: flex;
+       justify-content: space-between;
+        align-items: center;
+    }
+    span{
+        margin-left:20px;
+    }
+    span:hover{
+        color:#ccc
     }
 </style>
