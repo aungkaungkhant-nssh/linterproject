fa1c9aa4b59abdbcd32f44d77c5959b2188013b8
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-03-06 20:35:21 +0630

    add new project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="nav">
-    
-  </div>
+  <Navbar></Navbar>
   <router-view/>
 </template>
+<script>
+import Navbar from './components/Navbar'
+export default {
+  components: { Navbar },
 
+}
+</script>
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
